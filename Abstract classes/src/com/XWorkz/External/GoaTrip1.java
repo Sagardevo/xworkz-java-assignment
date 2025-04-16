f236544645e9f9c94acd9f1a64de28f04bdbb95f
sagar d <sagardmogaveer@gmail.com>
2025-04-16 09:01:16 +0530

    Updating assignment

---
+++ Abstract classes/src/com/XWorkz/External/GoaTrip1.java
@@ -0,0 +1,13 @@
+package com.XWorkz.External;
+
+public abstract class GoaTrip1 {
+
+
+    public GoaTrip1() {
+        System.out.println("No args Constructor");
+    }
+    public abstract void tripPlan1();
+    {
+        System.out.println("Goa Trip constructor from GoaTrip1");
+    }
+}
